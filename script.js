17f17358e1611e9ff7a9655738c47f38309feb38
Tom <tom@Toms-MacBook-Pro.local>
2019-08-30 15:14:30 -0700

    bunch of changes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var numOfClicks = 0;
+
 function fImageReveal() {
-    document.getElementById("fImage").style.display = "block"
+    numOfClicks++;
+    if (numOfClicks === 1) {
+        document.getElementById("fImage").style.display = "inline-block";
+    } else if (numOfClicks === 2) {
+        document.getElementById("sImage").style.display = "inline-block";
+    } else {
+        document.getElementById("tImage").style.display = "inline-block";
+    }
+    
+}
+
+function hottieReveal() {
+    document.getElementById("hottie").style.display = "block";
 }
